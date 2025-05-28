99646bbbc1c0fb04cc0bbd16b28a081c576c2e3a
lionelBatt <lionel.batt@gmail.com>
2025-05-28 14:17:51 +0200

    test

--- src/main/java/com/app/resto/controller/PasswordResetController.java
+++ src/main/java/com/app/resto/controller/PasswordResetController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import com.app.resto.service.PasswordResetService;
 
 @RestController
 @RequestMapping("/api/password-recovery")
+@CrossOrigin(origins = "*", maxAge = 3600)
 public class PasswordResetController {
 	
 	@Autowired

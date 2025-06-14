ec2013630112667075949e3cc6f0b5936f82e8ac
lionelBatt <lionel.batt@gmail.com>
2025-06-14 14:31:21 +0200

    changes on repository temporary

--- src/main/java/com/app/travel/repos/OrderRepository.java
+++ src/main/java/com/app/travel/repos/OrderRepository.java
@@ -4,8 +4,6 @@ import java.sql.Date;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import com.app.travel.model.Order;
 
@@ -18,13 +16,13 @@ public interface OrderRepository extends JpaRepository<Order, Integer>{
     List<Order> findByUserUserIdAndCreationDateAfter(Integer userId, Date date);
     List<Order> findByUserUserIdAndTripStartDateAfter(Integer userId, Date date);
 
-    @Query("SELECT o FROM Order o WHERE o.user.userId = :userId")
-    List<Order> findByUserId(@Param("userId") int userId);
+    //@Query("SELECT o FROM Order o WHERE o.user.userId = :userId")
+    //List<Order> findByUserId(@Param("userId") int userId);
 
-    @Query("select a from Order a where a.creationDate > :limit")
-    List<Order> findByCreationDateAfter(Date limit);
+    //@Query("select a from Order a where a.creationDate > :limit")
+    //List<Order> findByCreationDateAfter(Date limit);
 
-    @Query("select a from Order a where a.tripStartDate > :limit")
-    List<Order> findByTripStartDateAfter(Date limit);
+    //@Query("select a from Order a where a.tripStartDate > :limit")
+    //List<Order> findByTripStartDateAfter(Date limit);
 
 }
